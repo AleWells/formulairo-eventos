50e19348d4c2e0aa7fc2f7f165dc05238bd7f8b4
Ale Candia <alejandrogcandia@gmail.com>
2024-03-15 11:31:33 -0300

    se crea un dialog, y se realiza el button para cerrarlo, dentro del dialog va a esta el form para crear un usuario

--- src/component/Admin/CreateUser/CreateUser.jsx
+++ src/component/Admin/CreateUser/CreateUser.jsx
@@ -1,4 +1,34 @@
+import { Dialog ,Slide , AppBar, IconButton,Toolbar} from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close';
+import React from 'react';
 import styles from './CreateUser.module.css'
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
 export default function  CreateUser ({isOpen,handleClose}){
-    
+    const handleCloseButton = ()=>{
+        handleClose(false)
+    }
+   return <React.Fragment>
+    <Dialog
+    fullWidth
+    open={isOpen}
+    onClose={handleClose}
+    TransitionComponent={Transition}
+    >
+     <AppBar sx={{ position: 'relative' }}>
+    <Toolbar>
+    <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleCloseButton}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+           
+    </Toolbar>
+        </AppBar>    
+    </Dialog>
+   </React.Fragment>
 }
