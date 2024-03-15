f8cd079d6a6a93378cf752ba9de9a236abd75748
Ale Candia <alejandrogcandia@gmail.com>
2024-03-15 12:14:06 -0300

    se crea el formulario para la creación de un usuario

--- src/component/Admin/CreateUser/CreateUser.jsx
+++ src/component/Admin/CreateUser/CreateUser.jsx
@@ -2,6 +2,7 @@ import { Dialog ,Slide , AppBar, IconButton,Toolbar} from '@mui/material'
 import CloseIcon from '@mui/icons-material/Close';
 import React from 'react';
 import styles from './CreateUser.module.css'
+import FormCreateUser from './FormCreateUser/FormCreateUser';
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
   });
@@ -28,7 +29,8 @@ export default function  CreateUser ({isOpen,handleClose}){
             </IconButton>
            
     </Toolbar>
-        </AppBar>    
+        </AppBar>  
+    <FormCreateUser/>
     </Dialog>
    </React.Fragment>
 }
---
+++ src/component/Admin/CreateUser/FormCreateUser/FormCreateUser.jsx
@@ -0,0 +1,46 @@
+import styles from './FormCreateuser.module.css'
+import { TextField, Box, Typography, Button } from "@mui/material";
+const handleSubmit = (e)=>{
+    e.preventDefault();
+}
+const FormCreateUser = ()=>{
+return(<Box className={styles.form} component='form'  onSubmit={handleSubmit}>
+    <Typography variant='h6'>Compléta los datos</Typography>
+    <TextField
+    fullWidth
+    id="nombreCompleto"
+    label="Nombre Completo"
+    type='text'
+    variant="standard"
+    required
+  />
+  <TextField
+  fullWidth
+  id='email'
+  label='Correo Electrónico'
+  type='email'
+  variant='standard'
+  required
+  />
+
+  <TextField
+  fullWidth
+  id='password'
+  label='Contraseña'
+  type='password'
+  variant='standard'
+  required
+  />
+   <TextField
+  fullWidth
+  id='password'
+  label='Repetir Contraseña'
+  type='password'
+  variant='standard'
+  required
+  />
+
+<Button type='submit' variant='contained'>CREAR</Button>
+</Box>)
+}
+export default FormCreateUser;
