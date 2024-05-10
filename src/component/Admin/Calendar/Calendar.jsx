c0c85f9c657f4342fcf7b074e6c669d149769077
Ale Candia <alejandrogcandia@gmail.com>
2024-05-10 15:01:17 -0300

    se reconfigura componente Calendario, para que se visualice un selector en vez de que se vean todos los calendarios al mismo tiempo

--- src/component/Admin/Calendar/Calendar.jsx
+++ src/component/Admin/Calendar/Calendar.jsx
@@ -1,10 +1,11 @@
-import { Dialog, Slide, AppBar, IconButton, Toolbar, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Button } from '@mui/material';
+import { Dialog, Slide, AppBar, IconButton, Toolbar, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Button, MenuItem, Select, FormControl, InputLabel } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import React, { useState } from 'react';
 import styles from './Calendar.module.css';
 import { useSelector } from 'react-redux';
-import {compartirCalendario} from '../../../socket'
-import {alertPending} from '../../../services'
+import { compartirCalendario } from '../../../socket';
+import { alertPending } from '../../../services';
+
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
 });
@@ -15,33 +16,29 @@ export default function Calendar({ isOpen, handleClose }) {
         handleClose(false);
     };
 
-    // Estado inicial para el correo electrónico de cada calendario
-    const [emailInputs, setEmailInputs] = useState({});
+    const [selectedCalendar, setSelectedCalendar] = useState('');
+    const [emailInput, setEmailInput] = useState('');
 
-    const handleEmailInputChange = (event, calendarId) => {
-        // Actualiza el estado para el calendario correspondiente
-        setEmailInputs({
-            ...emailInputs,
-            [calendarId]: event.target.value
-        });
+    const handleEmailInputChange = (event) => {
+        setEmailInput(event.target.value);
     };
 
-    const handleAddEmail = (calendarId) => {
-        // Aquí puedes implementar la lógica para agregar el correo electrónico al calendario
-        console.log(`Agregando correo electrónico "${emailInputs[calendarId]}" al calendario con ID "${calendarId}"`);
-    
+    const handleAddEmail = () => {
+        if (!selectedCalendar || !emailInput) {
+            // Manejar caso en el que no se ha seleccionado un calendario o no se ha ingresado un correo electrónico
+            return;
+        }
 
         // Emitir un evento para conectar con el servidor
-        compartirCalendario(calendarId,emailInputs[calendarId]);
+        compartirCalendario(selectedCalendar, emailInput);
         alertPending();
-        // Limpia el campo de entrada después de agregar el correo electrónico
-        setEmailInputs({
-            ...emailInputs,
-            [calendarId]: '' // Limpia el campo de entrada para este calendario
-        });
+
+        // Limpiar campos después de agregar el correo electrónico
+        setSelectedCalendar('');
+        setEmailInput('');
+
         handleClose(false);
     };
-    
 
     return (
         <React.Fragment>
@@ -66,28 +63,41 @@ export default function Calendar({ isOpen, handleClose }) {
                 </AppBar>
                 <TableContainer>
                     <Table>
-                        <TableHead >
+                        <TableHead>
                             <TableRow>
-                                <TableCell style={{fontWeight:"bold"}}>Nombre del Calendario</TableCell>
-                                <TableCell style={{fontWeight:"bold"}}>Compartir por correo electrónico</TableCell>
+                                <TableCell style={{ fontWeight: "bold" }}>Nombre del Calendario</TableCell>
+                                <TableCell style={{ fontWeight: "bold" }}>Compartir por correo electrónico</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {calendarios.map((calendar) => (
-                                <TableRow key={calendar.id}>
-                                    <TableCell >{calendar.summary}</TableCell>
-                                    <TableCell style={{padding:12}} >
-                                        <TextField
-                                            style={{marginRight: 10}}
-                                            label="Correo electrónico"
-                                            variant="outlined"
-                                            value={emailInputs[calendar.id] || ''}
-                                            onChange={(event) => handleEmailInputChange(event, calendar.id)}
-                                        />
-                                        <Button style={{marginTop:10}} variant="contained" onClick={() => handleAddEmail(calendar.id)}>Agregar</Button>
-                                    </TableCell>
-                                </TableRow>
-                            ))}
+                            <TableRow>
+                                <TableCell>
+                                    <FormControl fullWidth>
+                                        <InputLabel id="calendar-selector-label">Seleccionar Calendario</InputLabel>
+                                        <Select
+                                            labelId="calendar-selector-label"
+                                            id="calendar-selector"
+                                            value={selectedCalendar}
+                                            onChange={(event) => setSelectedCalendar(event.target.value)}
+                                            label="Seleccionar Calendario"
+                                        >
+                                            {calendarios.map((calendar) => (
+                                                <MenuItem key={calendar.id} value={calendar.id}>{calendar.summary}</MenuItem>
+                                            ))}
+                                        </Select>
+                                    </FormControl>
+                                </TableCell>
+                                <TableCell style={{ padding: 12 }}>
+                                    <TextField
+                                        style={{ marginRight: 10 }}
+                                        label="Correo electrónico"
+                                        variant="outlined"
+                                        value={emailInput}
+                                        onChange={handleEmailInputChange}
+                                    />
+                                    <Button style={{ marginTop: 10 }} variant="contained" onClick={handleAddEmail}>Agregar</Button>
+                                </TableCell>
+                            </TableRow>
                         </TableBody>
                     </Table>
                 </TableContainer>
