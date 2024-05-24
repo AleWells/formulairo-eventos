59bdb4150986b0fe02e95537d14c3d10cb23c0e9
Ale Candia <alejandrogcandia@gmail.com>
2024-05-24 12:53:19 -0300

    se configura componente DashHome

--- src/component/DashHome/DashHome.jsx
+++ src/component/DashHome/DashHome.jsx
@@ -3,12 +3,14 @@ import styles from './DashHome.module.css'
 import { useSelector } from 'react-redux';
 import TableForms from '../TableForms/TableForms.jsx';
 import LoaderDash from '../LoaderDash/LoaderDash.jsx';
+import { useEffect } from 'react';
 
 
 export default function DashHome() {
 const {user,forms} = useSelector(state=>state.data)
 
-                                             
+useEffect(()=>{},[forms,user])  
+
   return (
     <Box className={styles.dashhome}>    
       {forms&&<TableForms forms={forms}/>}
