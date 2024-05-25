99bfb3cf7b95fc40cdbe045f99e94f91a868a5b2
Ale Candia <alejandrogcandia@gmail.com>
2024-05-25 19:09:56 -0300

    se crea alerta,y  configura el calendario para no poder seleccionar fines de semana ni fehcas anteriores

--- src/component/ViewCalendar/SelectedCalendar.jsx
+++ src/component/ViewCalendar/SelectedCalendar.jsx
@@ -3,38 +3,53 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import { Box } from '@mui/material';
-import styles from './SelectedCalendar.module.css'
+import styles from './SelectedCalendar.module.css';
+
+
 export default function ViewCalendar({ data }) {
   const calendarRef = useRef(null);
 
   const events = data?.map(item => ({
     title: item.summary,
     start: item.start.dateTime,
-    end: item.end.dateTime,    
+    end: item.end.dateTime,
   }));
 
   const handleDateClick = (info) => {
-    const calendarApi = calendarRef.current.getApi();
-    console.log(info.date)
+    const today = new Date().toISOString().split('T')[0];
+    const dayOfWeek = info.date.getUTCDay();
+    if (info.dateStr < today) {
+      alert("You cannot select a past date.");
+      return;
+    }
+    if (dayOfWeek === 0 || dayOfWeek === 6) {
+      alert("⚠️ No se puede seleccionar un fin de semana !","Error");
+
+      return;
+    }
+    console.log(info.date);
   };
 
-
+  const today = new Date().toISOString().split('T')[0];
 
   return (
-    <Box className={styles.calendar} >
+    <Box className={styles.calendar}>
       <FullCalendar
         ref={calendarRef}
         plugins={[dayGridPlugin, interactionPlugin]}
         initialView='dayGridMonth'
-        weekends={true}
+        weekends={true} // Para mostrar los fines de semana pero deshabilitar la selección
         events={events}
         dateClick={handleDateClick}
-     
+        validRange={{
+          start: today, // Deshabilita fechas anteriores a hoy
+        }}
         headerToolbar={{
           left: 'prev,next today',
           center: 'title',
           right: 'dayGridMonth,dayGridWeek,dayGridDay',
         }}
+        selectable={true} // Habilita la selección de fechas
       />
     </Box>
   );
--- src/services.js
+++ src/services.js
@@ -164,3 +164,4 @@ export function  alertDeleteEventsOk (){
     timer: 3000
   })
 }
+
