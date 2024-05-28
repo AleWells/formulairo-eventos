def5aa11689afcd5227bdd1202685e4f2d2be862
Ale Candia <alejandrogcandia@gmail.com>
2024-05-28 14:01:30 -0300

    se configura para que las horas reservadas aparezcan disable

--- src/component/DynamicForm/Body/Form/Form.jsx
+++ src/component/DynamicForm/Body/Form/Form.jsx
@@ -3,7 +3,7 @@ import { Dialog ,  Slide ,AppBar, Toolbar, IconButton} from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import RenderForm from './RenderForm/RenderForm';
 import { useDispatch , useSelector} from "react-redux";
-import { updateForm,uploadCalendarioSelecionado,uploadEventosCalendarioSeleccionado } from '../../../../redux/slice.js'
+import { updateForm,uploadAllFormsCalendarioSeleccionado,uploadCalendarioSelecionado,uploadEventosCalendarioSeleccionado } from '../../../../redux/slice.js'
 import { estructuraFormulario } from '../../../../redux/service';
 import { useNavigate } from 'react-router-dom';
 const Transition = React.forwardRef(function Transition(props, ref) {
@@ -23,6 +23,7 @@ export default function Form({isOpen,handleClose}){
        handleClose(false)   
        dispatch( uploadCalendarioSelecionado(null));
        dispatch(uploadEventosCalendarioSeleccionado(null))
+       dispatch(uploadAllFormsCalendarioSeleccionado(null))
        dispatch(updateForm(resetForm()))
        navigate('/');
        
--- src/component/SeleccionarFecha/component/Hora/Hora.jsx
+++ src/component/SeleccionarFecha/component/Hora/Hora.jsx
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import { Box, Typography, TextField, MenuItem, Button } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import styles from './Hora.module.css';
-import {updateForm} from '../../../../redux/slice'
+import { updateForm } from '../../../../redux/slice';
+
 const generarOpcionesDeTiempo = (inicio, fin, intervalo) => {
-  
     const opciones = [];
     let currentTime = new Date(`1970-01-01T${inicio}:00`);
     const endTime = new Date(`1970-01-01T${fin}:00`);
-    
+
     while (currentTime <= endTime) {
         const horas = String(currentTime.getHours()).padStart(2, '0');
         const minutos = String(currentTime.getMinutes()).padStart(2, '0');
@@ -19,9 +19,7 @@ const generarOpcionesDeTiempo = (inicio, fin, intervalo) => {
     return opciones;
 };
 
-const opcionesDeTiempo = generarOpcionesDeTiempo("08:30", "16:30", 15);
-
-export default function Hora({handleChangeSection}) {
+export default function Hora({ handleChangeSection }) {
     const dispatch = useDispatch();
     const { form, allFormsCalendarioSeleccionado } = useSelector(state => state.data);
     const [horaInicio, setHoraInicio] = useState("");
@@ -38,22 +36,49 @@ export default function Hora({handleChangeSection}) {
 
     // Filtra las opciones de hora final para que solo incluyan horas posteriores a la hora de inicio
     const opcionesDeHoraFinal = horaInicio
-        ? opcionesDeTiempo.filter(hora => hora > horaInicio)
+        ? generarOpcionesDeTiempo(horaInicio, "16:30", 15).filter(hora => hora > horaInicio)
         : [];
 
-    // se filtra formularios por fecha
-    const filtradoPorFecha = allFormsCalendarioSeleccionado.filter(item => {
-        // Obtenemos solo la parte de la fecha (YYYY-MM-DD) de item.fecha
-        const fechaItem = item.fecha.split('T')[0];
-        // Comparamos con la parte de la fecha de form.home.fecha
-        return fechaItem === form.home.fecha;
-    });
-    
-    console.log(filtradoPorFecha)
-    const handleButton = ()=>{
-        dispatch(updateForm({...form,home:{...form.home,horaInicio:horaInicio,horaFinal:horaFinal}}));
-        handleChangeSection('Home');
+    // Se filtra formularios por fecha
+    let opcionesDeTiempoFiltradas = generarOpcionesDeTiempo("08:30", "16:30", 15);
+
+    if (allFormsCalendarioSeleccionado) {
+        const filtradoPorFecha = allFormsCalendarioSeleccionado.filter(item => {
+            const fechaItem = item.fecha.split('T')[0];
+            return fechaItem === form.home.fecha;
+        });
+
+        const horariosReservados = filtradoPorFecha.reduce((reservados, formulario) => {
+            const horaInicio = formulario.horaInicio.split('T')[1].slice(0, 5); // Extraer solo la hora de inicio sin la fecha
+            const horaFinal = formulario.horaFinal.split('T')[1].slice(0, 5); // Extraer solo la hora final sin la fecha
+        
+            // Convertir las horas de inicio y final a minutos
+            const [inicioHH, inicioMM] = horaInicio.split(':').map(Number);
+            const [finalHH, finalMM] = horaFinal.split(':').map(Number);
+            const inicioMinutos = inicioHH * 60 + inicioMM;
+            const finalMinutos = finalHH * 60 + finalMM;
+        
+            // Generar horarios intermedios y agregarlos al array de reservados
+            for (let minutos = inicioMinutos; minutos < finalMinutos; minutos += 15) {
+                const horas = String(Math.floor(minutos / 60)).padStart(2, '0');
+                const minutosStr = String(minutos % 60).padStart(2, '0');
+                const hora = `${horas}:${minutosStr}`;
+                reservados.push(hora);
+            }
+            return reservados;
+        }, []);
+
+        opcionesDeTiempoFiltradas = opcionesDeTiempoFiltradas.map(hora => ({
+            hora,
+            reservado: horariosReservados.includes(hora)
+        }));
     }
+
+    const handleButton = () => {
+        dispatch(updateForm({ ...form, home: { ...form.home, horaInicio: horaInicio, horaFinal: horaFinal } }));
+        handleChangeSection('Home');
+    };
+
     return (
         <Box>
             <Typography>Horarios disponibles para el día : {form.home.fecha}</Typography>
@@ -67,9 +92,9 @@ export default function Hora({handleChangeSection}) {
                     margin="normal"
                     fullWidth
                 >
-                    {opcionesDeTiempo.map((opcion, index) => (
-                        <MenuItem key={index} value={opcion}>
-                            {opcion}
+                    {opcionesDeTiempoFiltradas.map((opcion, index) => (
+                        <MenuItem key={index} value={opcion.hora} disabled={opcion.reservado}>
+                            {opcion.hora}
                         </MenuItem>
                     ))}
                 </TextField>
